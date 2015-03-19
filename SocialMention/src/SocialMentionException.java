c0561bec398f4b9c1e06460e4f92f422a8dc9bb0
Clemens Banas <clemens.banas@me.com>
2015-03-19 10:59:59 +0100

    test code to query the social mention API

---
+++ SocialMention/src/SocialMentionException.java
@@ -0,0 +1,11 @@
+public class SocialMentionException extends Exception {
+
+	public SocialMentionException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public SocialMentionException(final String msg) {
+		super(msg);
+	}
+
+}
