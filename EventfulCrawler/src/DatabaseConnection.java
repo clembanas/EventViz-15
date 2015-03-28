3a6cba743929a76eeceb6bcc131acca864c447e0
Robert Bierbauer <Robert.Bierbauer@student.uibk.ac.at>
2015-03-28 10:52:06 +0100

    Eventful Crawler

---
+++ EventfulCrawler/src/DatabaseConnection.java
@@ -0,0 +1,259 @@
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseConnection {
+	private Connection conn = null;
+	private Statement stmt = null;
+	
+	public DatabaseConnection(){
+		connect();
+	}
+
+	public void connect() {
+		try {
+			Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance();
+			conn = DriverManager.getConnection("jdbc:derby:C:/temp/derby/Events/db;create=true");
+			stmt = conn.createStatement();
+			stmt.executeUpdate("Drop table events");
+		} catch (final SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		createTableCities();
+		createTableLocations();
+		createTableEvents();
+		createTableBands();
+		createTableArtists();
+		createTableEventPerformers();
+		createTableBandMembers();
+	}
+	
+	private void createTableCities(){
+		try{
+			final String createQ = "CREATE TABLE Cities(ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), " +
+									"NAME VARCHAR(255) NOT NULL, " +
+									"COUNTRY VARCHAR(255)," +
+									"POSTAL_CODE VARCHAR(16)," +
+									"LONGITUDE FLOAT," +
+									"LATITUDE FLOAT)";
+			stmt.executeUpdate(createQ);
+		}catch(SQLException e){
+			if(e.getSQLState().equals("X0Y32")) { return; }
+			else { e.printStackTrace(); }
+		}
+	}
+	
+	private void createTableLocations(){
+		try{
+			final String createQ = "CREATE TABLE Locations(ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)," +
+									"NAME VARCHAR(255) NOT NULL," +
+									"LONGITUDE FLOAT NOT NULL," +
+									"LATITUDE FLOAT NOT NULL," +
+									"CITY_ID INTEGER NOT NULL)";
+			stmt.executeUpdate(createQ);
+		}catch(SQLException e){
+			if(e.getSQLState().equals("X0Y32")) { return; }
+			else { e.printStackTrace(); }
+		}
+	}
+	
+	private void createTableEvents(){
+		try{
+			final String createQ = "CREATE TABLE Events(ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)," +
+									"NAME VARCHAR(255) NOT NULL," +
+									"DESCRIPTION VARCHAR(255)," +
+									"EVENT_TYPE VARCHAR(50)," +
+									"EVENTFUL_ID VARCHAR(50)," +
+									"LOCATION_ID INTEGER NOT NULL)";
+			stmt.executeUpdate(createQ);
+		}catch(SQLException e){
+			if(e.getSQLState().equals("X0Y32")) { return; }
+			else { e.printStackTrace(); }
+		}
+	}
+	
+	private void createTableBands(){
+		try{
+			final String createQ = "CREATE TABLE Bands(ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)," +
+									"NAME VARCHAR(255) NOT NULL)";
+			stmt.executeUpdate(createQ);
+		}catch(SQLException e){
+			if(e.getSQLState().equals("X0Y32")) { return; }
+			else { e.printStackTrace(); }
+		}
+	}
+	
+	private void createTableArtists(){
+		try{
+			final String createQ = "CREATE TABLE Artists(ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)," +
+									"NAME VARCHAR(255) NOT NULL)";
+			stmt.executeUpdate(createQ);
+		}catch(SQLException e){
+			if(e.getSQLState().equals("X0Y32")) { return; }
+			else { e.printStackTrace(); }
+		}
+	}
+	
+	private void createTableEventPerformers(){
+		try{
+			final String createQ = "CREATE TABLE EventPerformers(EVENT_ID INTEGER NOT NULL," +
+									"BAND_ID INTEGER NOT NULL)";
+			stmt.executeUpdate(createQ);
+		}catch(SQLException e){
+			if(e.getSQLState().equals("X0Y32")) { return; }
+			else { e.printStackTrace(); }
+		}
+	}
+	
+	private void createTableBandMembers(){
+		try{
+			final String createQ = "CREATE TABLE BandMembers(ARTIST_ID INTEGER NOT NULL," +
+									"BAND_ID INTEGER NOT NULL)";
+			stmt.executeUpdate(createQ);
+		}catch(SQLException e){
+			if(e.getSQLState().equals("X0Y32")) { return; }
+			else { e.printStackTrace(); }
+		}
+	}
+	
+	public int insertCity(String name, String country, String postal, double latitude, double longitude){
+		try{
+			PreparedStatement sta = conn.prepareStatement("SELECT id FROM Cities WHERE NAME = ? AND POSTAL_CODE = ?");
+			sta.setString(1, name);
+			sta.setString(2, postal);
+			ResultSet rs = sta.executeQuery();
+			if(!rs.next()){
+				sta = conn.prepareStatement("INSERT INTO Cities(name, country, postal_code, longitude, latitude) VALUES(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+				sta.setString(1, name);
+				sta.setString(2, country);
+				sta.setString(3, postal);
+				sta.setFloat(4, 0f);
+				sta.setFloat(5, 0f);
+				sta.executeUpdate();
+				rs = sta.getGeneratedKeys();
+				rs.next();
+				return rs.getInt(1);
+			}
+			else{
+				return rs.getInt("ID");
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public int insertLocation(String name, double longitude, double latitude, int cityID){
+		try{
+			PreparedStatement sta = conn.prepareStatement("SELECT id FROM Locations WHERE NAME = ? AND CITY_ID = ?");
+			sta.setString(1, name);
+			sta.setInt(2, cityID);
+			ResultSet rs = sta.executeQuery();
+			if(!rs.next()){
+				sta = conn.prepareStatement("INSERT INTO Locations(name, longitude, latitude, city_ID) VALUES(?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+				sta.setString(1, name);
+				sta.setDouble(2, longitude);
+				sta.setDouble(3, latitude);
+				sta.setInt(4, cityID);
+				sta.executeUpdate();
+				rs = sta.getGeneratedKeys();
+				rs.next();
+				return rs.getInt(1);
+			}
+			else{
+				return rs.getInt("ID");
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public int insertEvent(String name, String description, String eventType, String eventfulID, int locationID){
+		name = name.replaceAll("'", "''");
+		try{
+			PreparedStatement sta = conn.prepareStatement("SELECT id FROM Events WHERE EVENTFUL_ID = ?");
+			sta.setString(1, eventfulID);
+			ResultSet rs = sta.executeQuery();
+			if(!rs.next()){
+				sta = conn.prepareStatement("INSERT INTO Events(name, description, event_type, eventful_ID, location_ID) VALUES(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+				sta.setString(1, name);
+				sta.setString(2, description.substring(0, description.length() > 250 ? 250 : description.length()));
+				sta.setString(3, eventType);
+				sta.setString(4, eventfulID);
+				sta.setInt(5, locationID);
+				sta.executeUpdate();
+				rs = sta.getGeneratedKeys();
+				rs.next();
+				return rs.getInt(1);
+			}
+			else{
+				return rs.getInt("ID");
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public int insertBand(String name){
+		name = name.replaceAll("'", "''");
+		try{
+			PreparedStatement sta = conn.prepareStatement("SELECT id FROM Bands WHERE Name = ?");
+			sta.setString(1, name);
+			ResultSet rs = sta.executeQuery();
+			if(!rs.next()){
+				sta = conn.prepareStatement("INSERT INTO Bands(name) VALUES(?)", Statement.RETURN_GENERATED_KEYS);
+				sta.setString(1, name);
+				sta.executeUpdate();
+				rs = sta.getGeneratedKeys();
+				rs.next();
+				return rs.getInt(1);
+			}
+			else{
+				return rs.getInt("ID");
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public void insertEventPerformer(int eventId, int bandId){
+		try{
+			PreparedStatement sta = conn.prepareStatement("SELECT * FROM EventPerformers WHERE Event_ID = ? AND Band_ID = ?");
+			sta.setInt(1, eventId);
+			sta.setInt(2, bandId);
+			ResultSet rs = sta.executeQuery();
+			if(!rs.next()){
+				sta = conn.prepareStatement("INSERT INTO EventPerformers(Event_ID, Band_ID) VALUES(?, ?)", Statement.RETURN_GENERATED_KEYS);
+				sta.setInt(1, eventId);
+				sta.setInt(2, bandId);
+				sta.executeUpdate();
+				rs = sta.getGeneratedKeys();
+				rs.next();
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+	}
+
+	public void close(){
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ EventfulCrawler/src/Main.java
@@ -0,0 +1,50 @@
+import com.evdb.javaapi.APIConfiguration;
+import com.evdb.javaapi.EVDBAPIException;
+import com.evdb.javaapi.EVDBRuntimeException;
+import com.evdb.javaapi.data.Event;
+import com.evdb.javaapi.data.Group;
+import com.evdb.javaapi.data.Performer;
+import com.evdb.javaapi.data.SearchResult;
+import com.evdb.javaapi.data.request.EventSearchRequest;
+import com.evdb.javaapi.operations.EventOperations;
+
+
+public class Main {
+	public static void main(String args[]){
+		DatabaseConnection dbc = new DatabaseConnection();
+		int count = 0;
+		APIConfiguration.setEvdbUser("RobertBierbauer");
+		APIConfiguration.setEvdbPassword("3v3ntful");
+		APIConfiguration.setApiKey("T79WLsPLQkRDJZxv");
+
+		EventOperations eo = new EventOperations();
+		EventSearchRequest esr = new EventSearchRequest();
+
+		esr.setDateRange("2015030300-2015120400");
+		esr.setCategory("music");
+		esr.setPageSize(100);
+		SearchResult sr = null;
+		try {
+			for(int i = 1; i <= 2; i++){				
+				esr.setPageNumber(i);
+				sr = eo.search(esr);
+				for(int j = 0; j<sr.getEvents().size(); j++){
+					Event e = (Event)sr.getEvents().get(j);
+					int cityID = dbc.insertCity(e.getVenueCity(), e.getVenueCountry(), e.getVenuePostalCode(),  0, 0);
+					int locationID = dbc.insertLocation(e.getVenueName(), e.getVenueLongitude(), e.getVenueLatitude(), cityID);
+					int eventID = dbc.insertEvent(e.getTitle(), e.getDescription(), "", e.getSeid(), locationID);
+					if(e.getPerformers().size() > 0){
+						for(Performer p : e.getPerformers()){
+							int bandID = dbc.insertBand(p.getName());
+							dbc.insertEventPerformer(eventID, bandID);
+						}
+					}
+				}
+			}
+		}catch(EVDBRuntimeException var){
+			var.printStackTrace();
+		} catch( EVDBAPIException var){
+			var.printStackTrace();
+		}
+	}
+}
